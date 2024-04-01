da413240161f837e95d25c032345b481b9ec2b6b
Apurba Dutta <apurbadutta2099@gmail.com>
2024-04-01 20:23:01 +0530

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
       fontFamily: {
         customFont: ['Lilita One'],
       },
+      cursor: {
+        PointerCursor: 'url(./img/pointer.png), pointer',
+      },
     },
   },
   plugins: [],
